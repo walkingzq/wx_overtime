ab30ca783f5c9ed3a3dba780a0fe71b83fc223dd
walkingzq <walkingzq@163.com>
2017-09-13 17:09:18 +0800

    initial project version

---
+++ app.js
@@ -0,0 +1,40 @@
+//app.js
+//小程序的脚本代码。我们可以在这个文件中监听并处理小程序的生命周期函数、声明全局变量。
+App({
+  onLaunch: function () {//当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || [] //从本地缓存中获取key值为'logs'的value值，如果value不存在，则logs=[](一个js数组)
+    logs.unshift(Date.now())//插入时间到logs数组
+    wx.setStorageSync('logs', logs)//保存至本地缓存
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/form/form.js
@@ -0,0 +1,141 @@
+// pages/form/form.js
+Page({
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.showModal({
+      title: '提示',
+      content: '确认要提交吗？',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+          var that = this;
+          var formData = e.detail.value;
+          wx.request({
+            url: 'https://77205014.qcloud.la/form/formSubmitting',
+            method: "POST",
+            data: formData,
+            header: {
+              'Content-Type': 'application/json'
+            },
+            success: function (res) {
+              console.log(res.data)
+              if(res.data == "0"){
+              wx.showToast({
+                title: '提交成功',
+                icon: 'success',
+                duration: 2000
+              })
+              }else if(res.data == "-1"){
+                wx.navigateTo({
+                  url: '../message/message?message=部门填写有误，请检查后重新提交'
+                })
+              } else if (res.data == "-2") {
+                wx.navigateTo({
+                  url: '../message/message?message=日期填写有误，请检查后重新提交'
+                })
+              } else if (res.data == "-3") {
+                wx.navigateTo({
+                  url: '../message/message?message=姓名填写有误，请检查后重新提交'
+                })
+              } else if (res.data == "-4") {
+                wx.navigateTo({
+                  url: '../message/message?message=加班缘由填写有误，请检查后重新提交'
+                })
+              } else if (res.data == "-5") {
+                wx.navigateTo({
+                  url: '../message/message?message=加班时长填写有误，请检查后重新提交'
+                })
+              } else if (res.data == "-6") {
+                wx.navigateTo({
+                  url: '../message/message?message=加班地点填写有误，请检查后重新提交'
+                })
+              } else if (res.data == "-7") {
+                wx.navigateTo({
+                  url: '../message/message?message=系统异常，提交失败，请确认信息填写正确后重新提交或稍后重试'
+                })
+              }
+            },
+            fail:function(res){
+              wx.navigateTo({
+                url: '../message/message?message=系统异常，提交失败，请确认信息填写正确后重新提交或稍后重试'
+              })
+            }
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    });  
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
