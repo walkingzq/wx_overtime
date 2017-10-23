13f24df1104a10c49bc0f367af382158395bb1c7
walkingzq <walkingzq@163.com>
2017-10-23 18:06:18 +0800

    微调

--- app.js
+++ app.js
@@ -30,11 +30,31 @@ App({
               }
             }
           })
+        }else{
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success: res => {
+              wx.getUserInfo({
+                success: res => {
+                  console.log(res)
+                  // 可以将 res 发送给后台解码出 unionId
+                  this.globalData.userInfo = res.userInfo
+                  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                  // 所以此处加入 callback 以防止这种情况
+                  if (this.userInfoReadyCallback) {
+                    this.userInfoReadyCallback(res)
+                  }
+                }
+              })
+            }
+          })
         }
       }
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    encryptedData:null,
+    openid:null
   }
 })
